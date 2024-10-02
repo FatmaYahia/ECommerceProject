371a6e77020ab264794423cd15c60d7b33de2686
FatmaYahia <66412106+FatmaYahia@users.noreply.github.com>
2024-10-02 22:15:59 +0200

    App

---
+++ ECommerce/ECommerce.App/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace ECommerce.App.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly UnitOfWork UOW;
+        public ProductController(UnitOfWork UOW)
+        {
+            this.UOW = UOW;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProductDetails(int id)
+        {
+
+            return View(UOW.ProductRepository.GetById(id));
+        }
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+    }
+}
