224ca59b0dcedc477ae11fb0d991feace5807df4
FatmaYahia <66412106+FatmaYahia@users.noreply.github.com>
2025-03-02 15:12:15 +0200

    2-3-2024

--- ECommerce/ECommerce.App/Controllers/UserController.cs
+++ ECommerce/ECommerce.App/Controllers/UserController.cs
@@ -1,10 +1,17 @@
-﻿using Entity.AppModel;
+﻿using AutoMapper;
+using Entity.AppModel;
 using Microsoft.AspNetCore.Mvc;
+using Repository;
 
 namespace ECommerce.App.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UnitOfWork UOW;
+        public UserController(UnitOfWork uow)
+        {
+            UOW = uow;
+        }
         public IActionResult Index()
         {
             return View();
@@ -13,6 +20,19 @@ namespace ECommerce.App.Controllers
         {
             User user = new User();
             return View(user);
+        }
+        public IActionResult Register(User user)
+        {
+            try
+            {
+                UOW.UserRepository.CreateEntity(user);
+                UOW.UserRepository.Save();
+                }
+            catch
+            {
+
+            }
+            return View();
         }
         public IActionResult Login()
         {
